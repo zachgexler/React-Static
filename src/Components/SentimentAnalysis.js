0d43d5717f6f4f4e4df07a53d7d5ba2d823833ba
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-09-29 08:22:47 -0400

    Initial commit

---
+++ src/Components/SentimentAnalysis.js
@@ -0,0 +1,12 @@
+function SentientAnalysis(props) {
+  return (
+    <div className="SentAnalysis">
+      <h4>Sentient Analysis</h4>
+      <h5>{props.Num1}</h5>
+      <h5>{props.Num2}</h5>
+      <h5>{props.Num3}</h5>
+    </div>
+  );
+}
+
+export default SentientAnalysis;
